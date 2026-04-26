982f20e8abea660efc97f5ed904cba7ace9e88bf
Kip Cole <kipcole9@gmail.com>
2026-04-27 07:37:55 +1000

    Generate checksum script

--- checksum-Elixir.Ortex.Native.exs
+++ checksum-Elixir.Ortex.Native.exs
@@ -1,1 +1,12 @@
-Compiling 6 files (.ex)
+%{
+  "libortex-v0.1.11-pre4-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:1967ac52e663d5f60c117f7d9641950e6d1d8efe934f2bf1faa8e29b8a1937d7",
+  "libortex-v0.1.11-pre4-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:549a2b5a1ac48da4068e977192cc43e1577400ed4b3239349631a16d5aab564a",
+  "libortex-v0.1.11-pre4-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:83e69c3c03d2be939bd9c6ff0b891ab8a652cd5f3a452c65b9c22d710bbd7a31",
+  "libortex-v0.1.11-pre4-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:a5eef2346bc86958e5ea0b482006a40763b0473f28e3e11df22bff5de5626353",
+  "libortex-v0.1.11-pre4-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:b0e72fc80eedd15187664859b4a27b9b416bca5afed72df26df526f9a8579fb9",
+  "libortex-v0.1.11-pre4-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:345b4323b51594d4ffb9250f3d20b52a972f3629c5dda5c7f85ceb8b67f30bcb",
+  "libortex-v0.1.11-pre4-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:0882c975ebe6fa127f3f3597f28e793c974fa7c0a9ae3175ce88fbaa81d6515f",
+  "libortex-v0.1.11-pre4-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b271f2d5ed7d9a781cdeec2855ed9bc475812726e7de4f60f9455029c106f8e1",
+  "ortex-v0.1.11-pre4-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:0e40f02f6303cb890e02914a4ff3a124189ddd5b9069501707078061d6d139cb",
+  "ortex-v0.1.11-pre4-nif-2.17-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:33b11954ee7616f458f0230a7989af5798928d48d5ac26062182d9e82c8a8809",
+}
